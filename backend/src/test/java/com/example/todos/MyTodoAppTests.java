db24ca0397b71431142df081e77190658978ec01
Jesper Westin <jesperwestin@outlook.com>
2023-07-13 11:57:12 +0200

    added 3 tests

--- backend/src/test/java/com/example/todos/MyTodoAppTests.java
+++ backend/src/test/java/com/example/todos/MyTodoAppTests.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -15,7 +16,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -27,6 +28,7 @@ class MyTodoAppTests {
 
     @Autowired
     private MockMvc mvc;
+
     @Test
     void contextLoads() {
 
@@ -91,6 +93,61 @@ class MyTodoAppTests {
         });
 
         assertEquals(0, todos.size());
+    }
+
+    @Test
+    void shouldGetTodoItem() throws Exception {
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.get("/api/v1/todos/secure/1")
+                        .with(jwt().jwt(jwt -> jwt.subject("admin@outlook.com"))))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseBody = result.getResponse().getContentAsString();
+
+        ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+        Todo todo = objectMapper.readValue(responseBody, Todo.class);
+
+        assertNotNull(todo.getDescription());
+        assertNotNull(todo.getId());
+        assertNotNull(todo.getCreatedAt());
+        assertNotNull(todo.getUpdatedAt());
+        assertNotNull(todo.getUserEmail());
+        assertEquals("admin@outlook.com", todo.getUserEmail());
+    }
+
+    @Test
+    void shouldReturnErrorMessageForTodoNotFound() throws Exception {
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.get("/api/v1/todos/secure/20")
+                        .with(jwt().jwt(jwt -> jwt.subject("admin@outlook.com"))))
+                .andExpect(status().isNotFound())
+                .andReturn();
+
+        String responseBody = result.getResponse().getContentAsString();
+
+        assertTrue(responseBody.contains("Todo with id 20 not found."));
+    }
+    @Test
+    void shouldCreateTodo() throws Exception {
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.post("/api/v1/todos/secure")
+                        .with(jwt().jwt(jwt -> jwt.subject("adam@outlook.com")))
+                        .content("""
+                                {
+                                    "description": "testing!",
+                                    "done": false
+                                }""")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        String responseBody = result.getResponse().getContentAsString();
+        ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+
+        Todo todo = objectMapper.readValue(responseBody, Todo.class);
+
+        assertEquals("adam@outlook.com", todo.getUserEmail());
+        assertFalse(todo.isDone());
+        assertEquals("testing!", todo.getDescription());
+
     }
 }
 
